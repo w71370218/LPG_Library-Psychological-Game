5ea423351ab0e71c93c1f84362c6a6adf36d59e2
w71370218 <w7147370218@gmail.com>
2021-05-04 11:32:57 +0800

    11:32

--- LPG/trips/static/js/app.js
+++ LPG/trips/static/js/app.js
@@ -66,68 +66,7 @@ function submit() {
             },
         success: function(res) {
             console.log("Successfully sent the URL to Django");
-            console.log(json);
-            json = JSON.parse(json)
-            console.log(typeof json);
-
-            if (json.length == 1){
-                var newDiv = document.createElement("div");
-                var keyDiv = document.createElement("p");
-                keyDiv.innerHTML = json[0].message;
-                newDiv.appendChild(keyDiv);
-                document.getElementById("result_page").appendChild(newDiv);
-            } else {
-                var resultDiv = document.createElement("div");
-                resultDiv.id = "resultDiv";
-                var h3 = document.createElement("h3");
-                h3.innerHTML = "記得將結果截圖"
-                var p = document.createElement("p");
-                p.innerHTML = "借書時把結果給館員看";
-                var h32 = document.createElement("h3");
-                h32.innerHTML = "我們覺得你適合看這些書..."
-                resultDiv.appendChild(h3);
-                resultDiv.appendChild(p);
-                resultDiv.appendChild(h32);
-                document.getElementById("result_page").appendChild(resultDiv);
-
-                var bookDiv = document.createElement("div");
-                bookDiv.className = "bookDiv";
-                document.getElementById("result_page").appendChild(bookDiv);
-
-                for (var i = 0; i < json.length; i++) {          
-                    var newDiv = document.createElement("div");
-                    newDiv.id = (i+1).toString();
-                    newDiv.className = ""
-                    for (var j = 0; j < (Object.values(json[i].fields).length)-3; j++){
-                        fields = json[i].fields;
-                        var keyDiv = document.createElement("div");
-                        keyDiv.className = Object.keys(fields)[j].toString();
-                        var value = Object.values(fields)[j].toString();
-                        if (Object.keys(fields)[j] === 'picturename'){
-                            var key = document.createElement("img")
-                            key.src = value;
-                            key.className = "picture";
-                        } else {
-                            var key = document.createElement("span")
-                            key.innerHTML = value;
-                        }
-                        keyDiv.appendChild(key);
-                        newDiv.appendChild(keyDiv);
-                    };
-                    bookDiv.appendChild(newDiv);
-
-                    share_url = window.location.href.replace("app/","process_result_from_client/");
-                    fb_share_button = document.createElement("div");
-                    fb_share_button.className = "fb-share-button";
-                    now_url = window.location.href;
-                    fb_share_button.setAttribute('size', 'large');
-                    fb_share_button.setAttribute("data-layout", "button");
-                    fb_share_button.setAttribute("data-href", window.location.href.replace("app/","share_book/"+json[i].pk+"/og/"));
-                    newDiv.appendChild(fb_share_button)
-                    
-                    
-                };
-
+            $('#result_page').html(res);
                 (function(d, s, id) {
                         var js, fjs = d.getElementsByTagName(s)[0];
                         if (d.getElementById(id)) return;
@@ -135,19 +74,7 @@ function submit() {
                         js.src = "https://connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v3.0";
                         fjs.parentNode.insertBefore(js, fjs);
                     }(document, 'script', 'facebook-jssdk'));
-                //var recommend_button = document.createElement("button");
-                //recommend_button.innerHTML = "推薦書給我們";
 
-                const recommend_button = document.createElement("button");
-                
-
-                recommend_button.innerHTML = "推薦書給我們";
-
-                recommend_button.setAttribute('onclick', 'window.open(window.location.href.replace("app/","recommend/'+result.toString() +'","_blank"))');
-                recommend_button.setAttribute('class', "btn btn-outline-dark")
-                document.getElementById("result_page").appendChild(recommend_button);
-                }
-                
         },
         error: function(xhr,errmsg,err){
             console.log("Could not send URL to Django. Error: " + xhr.status + ": " + xhr.responseText);
